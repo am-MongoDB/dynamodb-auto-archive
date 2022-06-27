9f6bba6dff317604966a4bfb35b4cfda851a4153
am-MongoDB <andrew.morgan@mongodb.com>
2022-06-27 20:27:30 +0100

    Firehose data into S3

--- src/streamProcessor.js
+++ src/streamProcessor.js
@@ -1,3 +1,4 @@
+var AWS = require("aws-sdk");
 const getClient = require("./client").getClient;
 
 const COUNTER_KEY = "COUNTER"
@@ -6,6 +7,24 @@ exports.handler = async (event, context) => {
   const filteredRecords = event.Records.filter((record) => {
     return (record.eventName === "INSERT" || record.eventName === "MODIFY") &&
             record.dynamodb.Keys.ID.S !== COUNTER_KEY
+  })
+  let firehouse = new AWS.Firehose();
+  filteredRecords.forEach ( record => {
+    if (record.dynamodb && record.dynamodb.OldImage) {
+      console.log(`Sending this doc to stream: ${JSON.stringify(record.dynamodb.OldImage)}`);
+      let archiveEntry = {
+        Record: {Data: new Buffer(JSON.stringify(record.dynamodb.OldImage))},
+        DeliveryStreamName: process.env.FIREHOSE_NAME
+      };
+      firehouse.putRecord(archiveEntry, function (err, data) {
+        if (err) {
+            console.error("couldn't stream", err.stack);
+        }
+        else {
+            console.log(`INFO - successfully sent to Firehose: ${process.env.FIREHOSE_NAME}`);
+        }
+      });
+    }
   })
 
   await getClient().updateItem({
